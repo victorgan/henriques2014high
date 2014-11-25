171ce0357e8e66a6cbb546cad7613a91617dcb6d
Victor Gan <victorgan@gmail.com>
2014-11-25 15:03:20 -0800

    removed

--- workingDirectory/run_browse.m
+++
@@ -1,8 +0,0 @@
-% This script can be used to interactively inspect the results
-
-[sequences, experiments] = vot_environment();
-
-trackers = create_trackers('rpeTracker');
-
-vot_browse(trackers, sequences, experiments);
-
